843be13d4fe95a2d471ce728a53939cf61f1dbee
Dao Duong <daoduongtn2000@gmail.com>
2021-12-13 16:38:36 +0700

    upp

---
+++ b2buoi8.js
@@ -0,0 +1,8 @@
+var a = 10;
+var b = 18;
+
+var x = a--+b++-++b-++a;
+console.log(x);
+// Nhấn Run để chạy code
+// Thêm các dấu cách vào dòng 4 để code dễ đọc hơn
+// Giải thích tại sao kết quả lại như vậy. Gợi ý: Viết ra từng dòng để giải thích
